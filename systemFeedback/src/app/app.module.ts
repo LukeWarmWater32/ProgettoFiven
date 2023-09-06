82fbb20810776431e9c14b3d1ed6ed751c8aa724
Simonsp3 <porfido06@gmail.com>
2023-09-06 10:03:43 +0200

    Form Tag

--- systemFeedback/src/app/app.module.ts
+++ systemFeedback/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { TabellaComponent } from './tabella/tabella.component';
 import { UpdateTagComponent } from './update-tag/update-tag.component';
 import { TabellaTagComponent } from './tabella-tag/tabella-tag.component';
+import { FormTagComponent } from './form-tag/form-tag.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TabellaComponent,
     UpdateTagComponent,
-    TabellaTagComponent
+    TabellaTagComponent,
+    FormTagComponent
   ],
   imports: [
     BrowserModule,
---
+++ systemFeedback/src/app/form-tag/form-tag.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-tag',
+  templateUrl: './form-tag.component.html',
+  styleUrls: ['./form-tag.component.css']
+})
+export class FormTagComponent {
+
+}
